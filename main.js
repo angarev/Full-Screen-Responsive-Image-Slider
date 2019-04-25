f7853b24bb728f11f608f69971123df278afdbd3
preslyp <pancho.angarev@gmail.com>
2019-04-25 19:27:05 +0300

    Add next and prev functions to js

---
+++ main.js
@@ -0,0 +1,62 @@
+
+// Create NodeList with slides
+const slides = document.querySelectorAll('.slide');
+
+//Get buttons
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+
+//Option to start slider auto
+const auto = false;
+
+//Set interval in ms
+const intervalTime = 5000;
+
+let slideInterval;
+
+const nextSlide = () => {
+
+    //Get slide with current class
+    const current = document.querySelector('.current');
+
+    //Remove current class
+    current.classList.remove('current');
+
+    //Check for next slide
+    if (current.nextElementSibling) {
+        //Add current to next sibling
+        current.nextElementSibling.classList.add('current');
+    } else {
+        //Add current to start
+        slides[0].classList.add('current');
+    }
+
+    setTimeout(()=>current.classList.remove('current'));
+};
+
+
+const prevSlide = () => {
+
+    //Get slide with current class
+    const current = document.querySelector('.current');
+
+    //Remove current class
+    current.classList.remove('current');
+
+    //Check for next slide
+    if (current.previousElementSibling) {
+        //Add current to previous sibling
+        current.previousElementSibling.classList.add('current');
+    } else {
+        //Add current to last
+        slides[slides.length - 1].classList.add('current');
+    }
+
+    setTimeout(()=>current.classList.remove('current'));
+};
+
+
+//Add button events
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
